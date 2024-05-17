611b8864e1f4c42cf76f5f3fc9d5c266d13d2d7d
aniketjadhav16 <jadhavaniket897@gmail.com>
2024-05-17 20:53:17 +0530

    first commit

---
+++ Password-generator/src/components/Password.jsx
@@ -0,0 +1,86 @@
+import React, { useCallback, useEffect, useState } from "react";
+
+const Password = () => {
+    const [length, setLength] = useState(4);
+    const [numberAllowed, setNumberAllowed] = useState(false);
+    const [charAllowed, setCharAllowed] = useState(false);
+    const [password, setPassword] = useState("");
+
+    const passwordField = useCallback(() => {
+        let pass = "";
+        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        if (numberAllowed) str += "0123456789";
+        if (charAllowed) str += "<>?,./;:[]{}|`~!@#$%^&*()_-";
+
+        for (let i = 1; i <= length; i++) {
+            let char = Math.floor(Math.random() * str.length + 1);
+            pass += str.charAt(char);
+        }
+
+        setPassword(pass);
+    }, [length, numberAllowed, charAllowed]);
+
+    useEffect(() => {
+        passwordField();
+    }, [length, numberAllowed, charAllowed, passwordField]);
+
+    return (
+        <>
+            <h1 className="bg-gray-500 m-4 text-white text-xl p-4 rounded">Password Generator</h1>
+            <div className="mb-4">
+                <div className="flex items-center">
+                    <input
+                        type="text"
+                        className="border border-gray-300 p-2 rounded w-full"
+                        value={password}
+                        placeholder="Password"
+                        readOnly
+                    />
+                </div>
+            </div>
+
+            <div className="mb-4">
+                <div className="flex items-center">
+                    <input
+                        type="range"
+                        min={4}
+                        max={20}
+                        value={length}
+                        className="cursor-pointer mr-4"
+                        onChange={(e) => setLength(e.target.value)}
+                    />
+                    <label className="text-gray-700">Length: {length}</label>
+                </div>
+            </div>
+
+            <div className="mb-4">
+                <div className="flex items-center">
+                    <input
+                        type="checkbox"
+                        defaultChecked={numberAllowed}
+                        id="numberInput"
+                        className="mr-2"
+                        onChange={() => setNumberAllowed((prev) => !prev)}
+                    />
+                    <label htmlFor="numberInput" className="text-gray-700">Numbers</label>
+                </div>
+            </div>
+
+            <div className="mb-4">
+                <div className="flex items-center">
+                    <input
+                        type="checkbox"
+                        defaultChecked={charAllowed}
+                        id="charInput"
+                        className="mr-2"
+                        onChange={() => setCharAllowed((prev) => !prev)}
+                    />
+                    <label htmlFor="charInput" className="text-gray-700">Characters</label>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Password;
